4a1ddb51161b46d3df99238561ed0ec3016eef19
Karishma Daga <karishma.daga@queensu.ca>
2020-12-16 14:07:29 -0500

    official release info

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 
-import io
 from setuptools import setup
 from setuptools import find_packages
 
@@ -18,38 +17,40 @@ EXTRAS = {
 }
 
 CLASSIFIERS = [
-    "Intended Audience :: Science/Research",
-    "Topic :: Scientific/Engineering",
-    "Topic :: Scientific/Engineering :: Mathematics",
-    "Development Status :: 4 - Beta",
-    "Programming Language :: Python",
-    "Programming Language :: Python :: 3",
-    "Programming Language :: Python :: 3.4",
-    "Programming Language :: Python :: 3.5",
-    "Programming Language :: Python :: 3.6",
-    "Programming Language :: Python :: 3.7",
-    "Programming Language :: Python :: 3.8",
-    "Programming Language :: Python :: 3 :: Only",
+    'Intended Audience :: Science/Research',
+    'Topic :: Scientific/Engineering',
+    'Topic :: Scientific/Engineering :: Mathematics',
+    'Development Status :: 4 - Beta',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3 :: Only',
 ]
 
-with io.open('LICENSE', 'r', encoding='utf-8') as f:
+with open('LICENSE', 'r', encoding='utf-8') as f:
     LICENSE = f.read()
 
-with open("README.md") as f:
+with open('README.md', 'r', encoding='utf-8') as f:
     LONG_DESCRIPTION = f.read()
-
-LONG_DESCRIPTION_CONTENT_TYPE = "text/markdown"
 
 setup(
     name=NAME,
     version=VERSION,
-    description=DESCRIPTION,
-    long_description=LONG_DESCRIPTION,
-    long_description_content_type=LONG_DESCRIPTION_CONTENT_TYPE,
     classifiers=CLASSIFIERS,
     author='Karishma Daga, Christian Muise',
     author_email='karishma.daga@queensu.ca, christian.muise@queensu.ca',
-    license=LICENSE,
+    license="MIT",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    project_urls={
+        'Documentation': "https://bauhaus.readthedocs.io/",
+        'Source': "https://github.com/QuMuLab/bauhaus",
+    },
     python_requires='>=3.4',
     install_requires=DEPENDENCIES,
     extras_require=EXTRAS,
