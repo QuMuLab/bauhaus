15ee9dcc05f0fbb27639ed5798e692d38b454d13
beckydvn <rebeccaleahdevenezia@gmail.com>
2021-09-19 11:49:32 -0400

    get testing file working

--- tests/testing_new_features.py
+++ tests/testing_new_features.py
@@ -1,19 +1,30 @@
-from bauhaus.bauhaus.core import Encoding, proposition, constraint
+from bauhaus.core import Encoding, proposition
 from nnf import Var
 
-e = Encoding()
+if __name__ == "__main__":
+    e = Encoding()
 
-@proposition(e)
-class A(object):
-    def __init__(self, val):
-        self.val = val
+    @proposition(e)
+    class A(object):
+        def __init__(self, val):
+            self.val = val
 
-    def __repr__(self):
-        return f"A.{self.val}"
+        def __repr__(self):
+            return f"A.{self.val}"
 
-a, b, c, d = A(1), A(2), A(3), A(4)
-w, x, y, z = Var(1), Var(2), Var(3), Var(4)
-neg_var = ~w
-#imply_var = w >> x
+    # works
+    w, x, y, z = Var(1), Var(2), Var(3), Var(4)
+    imply_var = (w >> x) & (y | ~z)
 
-# e.add_constraint(x >> y)
+    # works
+    a, b, c, d = A(1), A(2), A(3), A(4)      
+    imply_prop = (a >> b) & (c | ~d)
+
+    e.add_constraint(imply_prop)
+    nnf_formula = e.compile()
+    nnf_formula = nnf_formula & imply_var
+    nnf_formula = nnf_formula.negate()
+
+    print()
+    print(nnf_formula.to_CNF())
+    print(nnf_formula.solve())
