3dc2c17999399a17129c0381d2d77b71cca264bf
beckydvn <18rldv@queensu.ca>
2022-01-03 18:53:17 -0500

    clean up tests

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,10 +1,9 @@
-from bauhaus.core import Encoding, constraint, proposition, And, Or
+from bauhaus.core import CustomNNF, Encoding, constraint, proposition, And, Or
 from bauhaus.constraint_builder import _ConstraintBuilder as cbuilder
 from nnf import Var
 import pytest
 """
 Unit tests for core functionalities and building constraints.
-
 See reference:
 https://github.com/mrocklin/multipledispatch/blob/master/multipledispatch/tests/test_core.py
 """
@@ -149,17 +148,108 @@ def test_failed_raw_constraints():
     with pytest.raises(TypeError):
         d.add_constraint(x | y)
 
-f = Encoding()
-
-@proposition(f)
-class G:
+g = Encoding()
+@proposition(g)
+class H:
     def __init__(self, data):
         self.data = data
-    def __repr__(self):
-        return self.data
 
-# test CustomNNF negate
-def test_custom_nnf_negate():
-    a, b, c = G("a"), G("b"), G("c")
-    assert ((a | b | c).negate()).compile().equivalent((~a & ~b & ~c).compile())
-    assert ((a & b & c).negate()).compile().equivalent((~a | ~b | ~c).compile())
+    def __repr__(self):
+        return str(self.data)
+
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return self.data == other.data
+
+    def __hash__(self):
+        return hash(self.data)
+
+def test_duplicates():
+
+    def clear():
+        g.clear_constraints()
+        g.clear_debug_constraints()
+        g._custom_constraints = set() 
+
+    # want to ensure h1 and h1_copy are treated the same
+    # generally, we want to ensure that no matter how many H(1) are created,
+    # if it is negated in another constraint or otherwise, they must all be
+    # treated the same.
+    h1 = H(1)
+    h1_copy = H(1)
+    h2 = H(2)
+    h3 = H(3)
+    h4 = H(4)
+    h1_copy_negated = ~h1_copy
+
+    # FIRST: testing custom constraints for duplication issues
+
+    # h1 and the negation of its copy both true in a single constraint
+    g.add_constraint(h1 & h1_copy_negated)
+    assert g.compile().solve() == None
+    clear()
+
+    # h1 and the negation of its copy both true with multiple constraints
+    g.add_constraint(h1)
+    g.add_constraint(h1_copy_negated)
+    assert g.compile().solve() == None
+    clear()
+
+    # same thing as above, trying a more complicated formula
+    g.add_constraint(h1 & (h2 | h4))
+    g.add_constraint(h1_copy_negated & (h3 | h4))
+    assert g.compile().solve() == None
+    clear()
+
+    # h1 and the negation of its copy both true through >> in a 
+    # single constraint
+    g.add_constraint((h1 >> h1_copy_negated) & h1)
+    assert g.compile().solve() == None
+    clear()
+
+    # h1 and the negation of its copy both true through >> through 
+    # multiple constraints
+    g.add_constraint(h1 >> h1_copy_negated)
+    g.add_constraint(h1)
+    assert g.compile().solve() == None
+    clear()
+
+    # NEXT: testing "bauhaus" constraints
+
+    # h1 and the negation of its copy both true in a single constraint
+    constraint.add_exactly_one(g, h1 & h1_copy_negated)
+    assert g.compile().solve() == None
+    clear()
+
+    # h1 and the negation of its copy both true with multiple constraints
+    constraint.add_exactly_one(g, h1)
+    constraint.add_exactly_one(g, h1_copy_negated)
+    assert g.compile().solve() == None
+    clear()
+
+    # same thing as above, trying a more complicated formula
+    constraint.add_exactly_one(g, h1 & (h2 | h4))
+    constraint.add_exactly_one(g, h1_copy_negated & (h3 | h4))
+    assert g.compile().solve() == None
+    clear()
+
+    # h1 and the negation of its copy both true through >> in a 
+    # single constraint
+    constraint.add_exactly_one(g, (h1 >> h1_copy_negated) & h1)
+    assert g.compile().solve() == None
+    clear()
+
+    # h1 and the negation of its copy both true through >> through 
+    # multiple constraints
+    constraint.add_exactly_one(g, h1 >> h1_copy_negated)
+    constraint.add_exactly_one(g, h1)
+    assert g.compile().solve() == None
+    clear()
+
+    # using the implication bauhaus method
+    constraint.add_implies_all(g, left=h1, right=h1_copy_negated)
+    constraint.add_exactly_one(g, h1)
+    assert g.compile().solve() == None
+    clear()
+
+test_duplicates()
